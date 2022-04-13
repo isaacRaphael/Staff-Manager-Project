33ba143a4a157edaaf20187ba189ee93f8b516b3
Raphael Isaac <isaacraphaelchidi@gmail.com>
2022-04-13 13:53:59 +0100

    seperating the imageservice

--- StaffManagement/Controllers/StaffController.cs
+++ StaffManagement/Controllers/StaffController.cs
@@ -21,17 +21,20 @@ namespace StaffManagement.Controllers
         private readonly SignInManager<Staff> _signInManager;
         private readonly UserManager<Staff> _userManager;
         private readonly IEmailService _emailSender;
+        private readonly IImageService _imageService;
 
         public StaffController(IStaffRepository staffRepository,
                                 SignInManager<Staff> signInManager,
                                  UserManager<Staff> userManager,
-                                 IEmailService emailSender
+                                 IEmailService emailSender,
+                                 IImageService imageService
                                 )
         {
             _staffRepository = staffRepository;
             _signInManager = signInManager;
             _userManager = userManager;
             _emailSender = emailSender;
+            _imageService = imageService;
         }
         //Login-Get
         public IActionResult Login()
@@ -93,23 +96,10 @@ namespace StaffManagement.Controllers
                 {
                     await model.Photo.CopyToAsync(stream);
                 }
-
-
-                var myAccount = new Account { ApiKey = "848314188379545", ApiSecret = "L9STxKQsLvitNYrdxBAhltnPLLk", Cloud = "dhfao0jm7" };
-                Cloudinary _cloudinary = new Cloudinary(myAccount);
-
-                var uploadParams = new ImageUploadParams()
-                {
-                    File = new FileDescription(filePath)
-                };
-
-                var uploadResult = await  _cloudinary.UploadAsync(uploadParams);
-                imgUrl = uploadResult.Url.AbsoluteUri;
+                var uploadResult = await  _imageService.AddImage(filePath);
+                imgUrl = uploadResult;
 
             }
-            
-
-
             var userWithEmail = _staffRepository.GetStaff(s => s.Email == model.Email);
             var userWithUserName = _staffRepository.GetStaff(s => s.UserName == model.UserName);
 
