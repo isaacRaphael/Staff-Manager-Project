ea04464cd8dca8c199cb0e4c921cb73fd375fb62
Raphael Isaac <isaacraphaelchidi@gmail.com>
2022-05-04 13:14:57 +0100

    refactor

--- StaffManagement/Startup.cs
+++ StaffManagement/Startup.cs
@@ -21,18 +21,21 @@ namespace StaffManagement
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
 
+        private readonly IWebHostEnvironment _env;
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services, IWebHostEnvironment env)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            if (env.IsDevelopment())
+            if (_env.IsDevelopment())
             {
               services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("default")));
             }
